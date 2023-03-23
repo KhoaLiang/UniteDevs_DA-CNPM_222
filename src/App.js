fbf9d402fb8185b99044308e6b784ff31fa2ecbe
trivien11vn <triviencoderr@gmail.com>
2023-03-23 22:07:18 +0700

    code trang quản lý tk nhân viên+khách hàng

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Manage_user from './component/Manage_user';
+function App() {
+  
+  return (
+    <div style={{padding:0}}>
+      <Manage_user />
+    </div>
+  );
+}
+
+export default App;
